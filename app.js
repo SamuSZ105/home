99996313096f5ca179bdacc7f81e89adc01501b6
SamuSZ105 <52174123+SamuSZ105@users.noreply.github.com>
2019-07-16 09:55:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -17,34 +17,6 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// function for loading each image via XHR
-
-function imgLoad(imgJSON) {
-  // return a promise for an image loading
-  return new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
-    request.open('GET', imgJSON.url);
-    request.responseType = 'blob';
-
-    request.onload = function() {
-      if (request.status == 200) {
-        var arrayResponse = [];
-        arrayResponse[0] = request.response;
-        arrayResponse[1] = imgJSON;
-        resolve(arrayResponse);
-      } else {
-        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
-      }
-    };
-
-    request.onerror = function() {
-      reject(Error('There was a network error.'));
-    };
-
-    // Send the request
-    request.send();
-  });
-}
 
 var imgSection = document.querySelector('section');
 
