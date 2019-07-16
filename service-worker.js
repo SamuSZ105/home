3bb1334e65a003eb240d76691245778c358158a6
SamuSZ105 <52174123+SamuSZ105@users.noreply.github.com>
2019-07-16 09:41:00 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,15 +2,16 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
+        '/home/',
+        '/home/index.html',
+        '/home/index.css',
+        '/home/app.js',
+        '/home/vendor.js',
+        '/home/vendor.css',
+        '/home/bootstrap/bootstrap.min.css',
+        '/home/bootstrap/bootstrap.min.js',
+        '/home/bootstrap/jquery-3.3.1.slim.min.js',
+        '/home/bootstrap/popper.min.js'
       ]);
     })
   );
