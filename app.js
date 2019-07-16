673dc820171307254677bc6e9752d8a8c5c20b14
SamuSZ105 <52174123+SamuSZ105@users.noreply.github.com>
2019-07-16 09:35:15 -0600

    Update app.js

--- app.js
+++ app.js
@@ -50,25 +50,7 @@ var imgSection = document.querySelector('section');
 
 window.onload = function() {
 
-  // load each set of image, alt text, name and caption
-  for(var i = 0; i<=Gallery.images.length-1; i++) {
-    imgLoad(Gallery.images[i]).then(function(arrayResponse) {
-
-      var myImage = document.createElement('img');
-      var myFigure = document.createElement('figure');
-      var myCaption = document.createElement('caption');
-      var imageURL = window.URL.createObjectURL(arrayResponse[0]);
-
-      myImage.src = imageURL;
-      myImage.setAttribute('alt', arrayResponse[1].alt);
-      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
-
-      imgSection.appendChild(myFigure);
-      myFigure.appendChild(myImage);
-      myFigure.appendChild(myCaption);
-
-    }, function(Error) {
-      console.log(Error);
-    });
+  // 
+  
   }
 };
